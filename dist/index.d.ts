2741b4c230a0d0b43ce5e8aea0e04e86314a6dc2
Gustaf Räntilä <g.rantila@gmail.com>
2018-12-02 18:02:58 +0100

    chore(package): Only ship dist folder, don't repo-store dist folder

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import * as u2fApi from './lib/u2f-api';
-export * from './lib/u2f-api';
-export default u2fApi;
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-var u2fApi = require("./lib/u2f-api");
-__export(require("./lib/u2f-api"));
-exports.default = u2fApi;
-//# sourceMappingURL=index.js.map
