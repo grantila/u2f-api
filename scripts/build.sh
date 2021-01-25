26f1cb6d161f15248d3c3cecf713cc74612844f8
Gustaf Räntilä <g.rantila@gmail.com>
2021-01-25 21:28:21 +0100

    fix(test): fixed build script warning

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,10 @@ patch google/google-u2f-api.js google/this.patch -o lib/generated-google-u2f-api
 
 node_modules/.bin/rimraf dist
 node_modules/.bin/tsc -p .
-find test lib -name '*.js' | cpio -pdm dist/
+find lib -name '*.js' | cpio -pdm dist/
 
 patch lib/generated-google-u2f-api.js < google/modules.patch
 
 node_modules/.bin/rimraf dist-bundle
 node_modules/.bin/tsc -p tsconfig.bundle.json
-find test lib -name '*.js' | cpio -pdm dist-bundle/
+find lib -name '*.js' | cpio -pdm dist-bundle/
